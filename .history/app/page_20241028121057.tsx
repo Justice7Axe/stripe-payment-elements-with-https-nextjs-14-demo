666bcfd7a90e6de61020df514a783749c970c044
Justice7Axe <justice7axe@gmail.com>
2024-10-28 12:28:24 +0900

    11

--- .history/app/page_20241028121057.tsx
+++ .history/app/page_20241028121057.tsx
@@ -17,7 +17,7 @@ export default function Home() {
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
       <div className="mb-10">
         <h1 className="text-4xl font-extrabold mb-2">
-          Jaewoo&apos;s private transfer booking
+          Jaewoo&apos;s private transfer book
         </h1>
         <h2 className="text-2xl">
           has requested
